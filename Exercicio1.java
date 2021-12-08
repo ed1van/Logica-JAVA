590db2906e28424b4901cccafb9d2c5ef61e42aa
ed1van <95690812+ed1van@users.noreply.github.com>
2021-12-08 19:18:44 -0300

    Update Exercicio1.java

--- Exercicio1.java
+++ Exercicio1.java
@@ -6,16 +6,17 @@ public class Exercicio1 {
 
 	public static void main(String[] args) {
 		
-		int anos, meses, dias, idaded;
+		Scanner ler = new Scanner( System.in );
+		int anos, meses=12, dias=365, idaded;
 		
-		meses = 12;
-		dias = 365;
-		anos = 5;
+		System.out.print("Digite sua idade: ");
+		anos = ler.nextInt();
 		
 		
 		idaded = anos * 365;
 		
-		System.out.println("Você tem "+idaded+ " dias de vida");
+		System.out.println("VocÃª tem "+idaded+ " dias de vida");
+
 
 	}
 
